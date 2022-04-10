9c1e06d74938fc7de540f723a62ee10e57009685
Vyacheslav Mityagin <mityaginvs@yandex.ru>
2022-04-10 15:54:09 +0300

    features: карточки на странице добавлены через js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,60 @@
-let changeProfileIcon = document.querySelector ('.profile__edit-button');
-let EditorProfile = document.querySelector ('.popup');
-let IconClosePopup = document.querySelector ('.form__button_type_cross');
-let ProfileNameText = document.querySelector ('.profile__name');
-let ProfileCareerText = document.querySelector ('.profile__career');
-let ProfileNameInput = document.querySelector ('.form__input_type_name');
-let ProfileCareerInput = document.querySelector ('.form__input_type_career');
+const changeProfileIcon = document.querySelector ('.profile__edit-button');
+const EditorProfile = document.querySelector ('.popup');
+const IconClosePopup = document.querySelector ('.form__button_type_cross');
+const ProfileNameText = document.querySelector ('.profile__name');
+const ProfileCareerText = document.querySelector ('.profile__career');
+const ProfileNameInput = document.querySelector ('.form__input_type_name');
+const ProfileCareerInput = document.querySelector ('.form__input_type_career');
+
+const listElements = document.querySelector ('.elements');
+const templateSixBoxes = document.querySelector ('.template__six-boxes');
+const initialCards = [
+  {
+    name: 'Дагестан',
+    link: './images/elements__image_dagestan.jpg'
+  },
+  {
+    name: 'Сочи',
+    link: './images/elements__image_sochi.jpg'
+  },
+  {
+    name: 'Успенский монастырь',
+    link: './images/elements__image_uspenskii-monastir.jpg'
+  },
+  {
+    name: 'Москва',
+    link: './images/elements__image_moscow.jpg'
+  },
+  {
+    name: 'Самара',
+    link: './images/elements__image_shiryaevo.jpg'
+  },
+  {
+    name: 'Карачаево-Черкесия',
+    link: './images/elements__image_karachaeva-cherkesiya.jpg'
+  },
+];
+
+/* Добавляет карточку на страницу из массива initialCards c помощью функции getCard*/
+function render() {
+  const html = initialCards.map(getCard);
+  listElements.append(...html);
+}
+
+/* Копирует из HTML темплейт и возвращает элемент li с содержимым + заполняет данными из массива */
+function getCard(item) {
+  const getTemplateSixBoxes = templateSixBoxes.content.cloneNode(true);
+
+  const titleCard = getTemplateSixBoxes.querySelector('.element__title');
+  titleCard.textContent = item.name;
+
+  const ImageCard = getTemplateSixBoxes.querySelector('.element__image');
+  ImageCard.src = item.link;
+
+  return getTemplateSixBoxes;
+}
+
+render();
 
 function toggleWindowPopup() {
   ProfileNameInput.value = ProfileNameText.textContent;
@@ -22,21 +72,21 @@ function onOverlayClick(event) {
 }
 EditorProfile.addEventListener('click', onOverlayClick);
 
-let formElement = document.querySelector ('.form');// Воспользуйтесь методом querySelector()
+const formElement = document.querySelector ('.form');// Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = document.querySelector ('.form__input_type_name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector ('.form__input_type_career'); // Воспользуйтесь инструментом .querySelector()
+const nameInput = document.querySelector ('.form__input_type_name');// Воспользуйтесь инструментом .querySelector()
+const jobInput = document.querySelector ('.form__input_type_career'); // Воспользуйтесь инструментом .querySelector()
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 // Так мы можем определить свою логику отправки.
 // О том, как это делать, расскажем позже.
-    let profileNameForm = nameInput.value;
-    let profileCareerForm = jobInput.value;
+    const profileNameForm = nameInput.value;
+    const profileCareerForm = jobInput.value;
     // Получите значение полей jobInput и nameInput из свойства value
-    let profileNameStroke = document.querySelector('.profile__name');
-    let profileCareerStroke = document.querySelector('.profile__career');
+    const profileNameStroke = document.querySelector('.profile__name');
+    const profileCareerStroke = document.querySelector('.profile__career');
     // Выберите элементы, куда должны быть вставлены значения полей
     profileNameStroke.textContent = profileNameForm;
     profileCareerStroke.textContent = profileCareerForm;
