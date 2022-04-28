bbf152dd25fae870672035f336eeb3cd0e04c2b9
Vyacheslav Mityagin <mityaginvs@yandex.ru>
2022-04-28 22:58:57 +0300

    fix: код валидации вынесен в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -179,67 +179,6 @@ function removeCardElement(event) {
 iconCrossClosePopupImage.addEventListener('click', () => {closeWindowPopup(popupTypeImage)});
 popupTypeImage.addEventListener('click',  () => onOverlayClick(popupTypeImage, event));
 
-
-function showInputError(formElement, inputElement, errorMessage, {inputErrorClass, errorClass}) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(inputErrorClass);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(errorClass);
-}
-
-function hideInputError(formElement,inputElement, {inputErrorClass, errorClass}) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(inputErrorClass);
-  errorElement.classList.remove(errorClass);
-  errorElement.textContent = '';
-}
-
-function isValid(formElement,inputElement, {...rest}) {
-  if(!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, rest);
-  } else {
-    hideInputError(formElement,inputElement, rest);
-  }
-}
-
-function setEventListener(formElement, {inputSelector, submitButtonSelector, ...rest}) {
-  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
-  const buttonElement = formElement.querySelector(submitButtonSelector);
-
-  toggleButtonState(inputList, buttonElement, rest);
-
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function() {
-      toggleButtonState(inputList, buttonElement, rest);
-      isValid(formElement, inputElement, rest);
-    });
-  });
-};
-
-function enableValidation({formSelector, ...rest}) {
-  const formList = Array.from(document.querySelectorAll(formSelector));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-    });
-    setEventListener(formElement, rest);
-  });
-};
-
-function hasInvalidInput(inputList) {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-}
-
-function toggleButtonState(inputList, buttonElement, {inactiveButtonClass}) {
-    if(hasInvalidInput(inputList)) {
-      buttonElement.classList.add(inactiveButtonClass);
-    } else {
-      buttonElement.classList.remove(inactiveButtonClass);
-    }
-};
-
 formElementEditProfile.addEventListener('submit', () => {
   enableValidation({
     formSelector: '.form',
